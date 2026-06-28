3ba00c4a87361160ec9f361a5909db9700e38876
lwm <2997504841@qq.com>
2026-06-28 10:01:43 +0800

    优化写代码体验：启动时，默认将“测试视频文件路径”添加到标题栏

--- main.cpp
+++ main.cpp
@@ -11,5 +11,11 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+
+    // 启动时，默认将“测试视频文件路径”添加到标题栏
+    QFileInfo testFile("C:\\Users\\lwm\\Desktop\\352x288_25fps.mp4");
+    if(testFile.exists())
+        w.setWindowTitle(testFile.absoluteFilePath());
+
     return a.exec();
 }
