eabbc3395ab817dac967151550262b332b82e5a3
lwm <2997504841@qq.com>
2026-06-28 17:10:54 +0800

    完成swr_cvt_pcm()，未测试，未调用。

--- MyAudioDecodeThread.h
+++ MyAudioDecodeThread.h
@@ -8,8 +8,12 @@ extern "C" {
 #include <libavcodec/avcodec.h>
 #include <libavformat/avformat.h>
 #include <libswscale/swscale.h>  // （视频）必须包含这个头文件
+
+#include <libavutil/opt.h>
+#include <libswresample/swresample.h>
 }
 
+#include <QDebug>
 #include <QThread>
 #include <SDL.h>
 struct FFmpegPlayerCtx;
@@ -32,6 +36,86 @@ public:
     int decode_packet(AVCodecContext *dec, const AVPacket *pkt ,AVFrame *frame);
 
     void getAudioData(unsigned char *stream, int len);
+
+    int swr_cvt_pcm(AVFrame *src, QByteArray &dst,
+                    const int dst_channels = 2,
+                    const int dst_freq_sample_rate = 48000,
+                    const enum AVSampleFormat dst_sample_fmt = AV_SAMPLE_FMT_S16)
+    {
+        /* 第1步、init变量 swr_ctx */
+        SwrContext *swr_ctx = nullptr;
+        swr_ctx = swr_alloc();//ffmpeg文档：与libavcodec和libavformat不同，此结构是不透明的。这意味着，如果您想设置选项，必须使用AVOptions API，而不能直接为该结构的成员设置值。
+        //输入音频格式，直接用音频解码上下文的参数即可
+        // av_opt_set_chlayout(swr_ctx, "in_chlayout", &is->audio_dec_ctx->ch_layout, 0);
+        // av_opt_set_int(swr_ctx, "in_sample_rate",       is->audio_dec_ctx->sample_rate, 0);
+        // av_opt_set_sample_fmt(swr_ctx, "in_sample_fmt", is->audio_dec_ctx->sample_fmt, 0);
+        //不使用AVCodecContext而是使用AVFrame的参数，这样就不需要输入音频解码上下文
+        av_opt_set_chlayout(swr_ctx, "in_chlayout", &src->ch_layout, 0);
+        av_opt_set_int(swr_ctx, "in_sample_rate",       src->sample_rate, 0);
+        av_opt_set_sample_fmt(swr_ctx, "in_sample_fmt", static_cast<AVSampleFormat>(src->format), 0);//需要强转：dectx分为sample_fmt/pix_fmt(enum)区分音频视频，而AVFrame不区分:format(int)
+
+        AVChannelLayout outLayout;
+        // use stereo
+        av_channel_layout_default(&outLayout, dst_channels);
+        //输入音频格式，建议在FFmpegPlayerCtx声明输出格式，而不是使用魔法数字
+        av_opt_set_chlayout(swr_ctx, "out_chlayout", &outLayout, 0);
+        av_opt_set_int(swr_ctx, "out_sample_rate",       dst_freq_sample_rate, 0);
+        av_opt_set_sample_fmt(swr_ctx, "out_sample_fmt", dst_sample_fmt, 0);
+        swr_init(swr_ctx);
+
+        /* 第2步、转换音频格式为目标格式 */
+        // 确定dst的每个采样点的位深
+        int dst_bytes_per_sample = -1;
+        switch (dst_sample_fmt) {
+        case AV_SAMPLE_FMT_U8:{
+            dst_bytes_per_sample = 1;// 8bit
+            break;
+        }
+        case AV_SAMPLE_FMT_S16:{
+            dst_bytes_per_sample = 2;// 16bit
+            break;
+        }
+        case AV_SAMPLE_FMT_S32:{
+            dst_bytes_per_sample = 4;// 32bit
+            break;
+        }
+        default:
+            break;
+        }
+        if(dst_bytes_per_sample == -1){
+            qDebug()<<"swr_cvt_pcm()错误：未知dst_sample_fmt";
+            return -1;
+        }
+
+        //swr_convert()文档：如果输入的数据量超过输出空间，则输入数据将被缓冲。
+        //                  您可以通过使用swr_get_out_samples()函数来获取给定输入样本数所需输出样本数的上限，从而避免这种缓冲。
+        int upper_bound_samples_per_channel = swr_get_out_samples(swr_ctx, src->nb_samples);
+        uint8_t *out[4] = {0};
+        int upper_bound_len = upper_bound_samples_per_channel * dst_bytes_per_sample * dst_channels;
+        out[0] = (uint8_t*)av_malloc(upper_bound_len);
+        // number of samples output per channel
+        int samples = swr_convert(swr_ctx,
+                                  out,
+                                  upper_bound_samples_per_channel,//每个通道可用的输出采样点 amount of space available for output in samples per channel
+                                  (const uint8_t**)src->data,
+                                  src->nb_samples
+                                  );
+        if (samples > 0) {
+            // memcpy(is->audio_buf, out[0], samples * 2 * 2);
+            // 入队，数据拷贝到audio_buf
+            // is->audio_buf_q.enqueue((const char*)out[0],
+            //                         samples * dst_bytes * dst_channels);
+            int len = samples * dst_bytes_per_sample * dst_channels;
+            dst.append((const char*)out[0], len);
+        }
+        /* 第3步、释放局部资源 */
+        av_free(out[0]);
+        /* 第4步、释放全局资源 */
+        swr_free(&swr_ctx);
+        swr_ctx = nullptr;
+
+        return 0;
+    }
 
 signals:
     void sendMessage();
